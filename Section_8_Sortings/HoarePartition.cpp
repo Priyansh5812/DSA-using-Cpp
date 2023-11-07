090eadd8aa9833757d0d42ba1943d9137f6cc043
Priyansh5812 <79259401+Priyansh5812@users.noreply.github.com>
2023-11-07 16:31:57 +0530

    Create HoarePartition.cpp

---
+++ Section_8_Sortings/HoarePartition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{   /*
+        Partition metjod which is generally used in Quick Sort.
+    */
+
+   vector<int>arr = {5,8,1,2,1,0,6,2};
+   
+
+    return 0;
+}
