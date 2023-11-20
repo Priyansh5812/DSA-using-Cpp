3fe1e611c375c1663f0920bbf3a8b77d3dcc341b
Priyansh5812 <79259401+Priyansh5812@users.noreply.github.com>
2023-11-20 10:03:27 +0530

    as

--- Section_8_Sortings/HoarePartition.cpp
+++ Section_8_Sortings/HoarePartition.cpp
@@ -2,16 +2,7 @@
 #include <vector>
 using namespace std;
 
-<<<<<<< HEAD
-int main()
-{   /*
-        Partition metjod which is generally used in Quick Sort.
-    */
 
-   vector<int>arr = {5,8,1,2,1,0,6,2};
-   
-
-=======
 
 void Partition(vector<int>& arr , int l , int h)
 {
@@ -49,6 +40,6 @@ int main()
     {
         cout << i << " ";
     }
->>>>>>> 3c6c29f822aa4dcc14f2aa5f7d96cb646011fa1d
+
     return 0;
 }
