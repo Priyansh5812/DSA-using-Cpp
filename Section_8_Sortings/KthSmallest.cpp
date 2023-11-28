13cc494e0afa22e7f6cee5f2c4589d94f6ff9502
Cpp4 <79259401+Priyansh5812@users.noreply.github.com>
2023-11-28 12:16:19 +0530

    Update

--- Section_8_Sortings/KthSmallest.cpp
+++ Section_8_Sortings/KthSmallest.cpp
@@ -1,7 +1,52 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main()
+int Partition(vector<int> & arr , int l , int h)
 {
+    int i = l;
+    int j = h;
+    int pivot = arr[l];
+
+    while(i < j)
+    {
+        do
+        {
+            i++;
+        } while (arr[i] < pivot);
+        
+        do
+        {
+            j--;
+        } while (arr[j] > pivot);
+
+        if(i < j)
+        {
+            swap(arr[i] , arr[j]);
+        }
+
+    }
+    swap(arr[j] , arr[l]);
+    return j;
+}
+
+
+void GetKthElement(vector<int>& arr , int l , int h , int k)
+{
+
+    if(l < h)
+    {
+        
+    }
+
+}
+
+
+
+
+int main()
+{   
+    vector<int> arr = {5,8,1,2,0,3,9,8};
+
     return 0;
 }
